45b8b1b35a4b572360145e7905236c4aa73099eb
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2025-09-17 12:01:47 +0300

    Add class Item in models.py

---
+++ restaurant_menu/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main dishes", "Main Dishes"),
+    ("desserts", "Desserts")
+)
+
+STATUS = (
+    (0, "Unavailable"),
+    (1, "Available")
+)
+
+class Item(models.Model):
+    meal_type = models.CharField(max_length=1000, choices=MEAL_TYPE)
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
